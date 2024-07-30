2192a73649f67b9994223d1bb82f746c365cb54e
zamzami ahmad <ahmadzamzami875@gmail.com>
2024-07-30 08:32:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,25 +11,17 @@ function generatekhodam(nama){
     const khodams = [
         'Buaya darat &#x1F4A9;',
         'Raja Iblis &#x1F480;',
-        'Tukang Kentut &#x1F639;',
-        'Elang Papua &#x1F480;',
-        'Bocil ingusan &#x1F4A9;',
         'Roh Jahat &#x1F480;',
         'Ayam Jantan &#x1F639;',
-        'Roger Sumatera &#x1F639;',
-        'Biawak Jawa &#x1F480;',
         'Makhluk Hytam &#x1F480;',
         'Anomali &#x1F480;',
         'Orang Ganteng &#x1F480;',
         'Tunggu Kiris &#x1F480;',
-        'Leviathan &#x1F480;',
         'Wibu Nolep &#x1F4A9;',
-        'Iblis Keren &#x1F480;',
         'Ksatria Sigma &#x1F917;',
         'Pangeran Kegelapan &#x1F480;',
         'Si Imut &#x1F917;',
-        'Monyet Biru &#x1F639;',
-        'Kucing Mewing &#x1F917;;'
+        'Kucing Mewing &#x1F917;'
     ];
 
     const index = nama.length % khodams.length;
